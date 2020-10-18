039ac693269ba798ecdf79a3fa21e8f10dbf576e
wjwarehouse <73050267+wjwarehouse@users.noreply.github.com>
2020-10-18 15:24:41 +0800

    上传文件
    随便上传的文件

---
+++ Retangle.java
@@ -0,0 +1,30 @@
+package my;
+import java.util.Scanner;
+public class Retangle {
+        private double length;
+        private double width;
+        private double area;
+        public Retangle(double a,double b){
+            this.length=a;
+            this.width=b;
+        }
+
+        public double getArea(double length,double width){
+            area=length*width;
+            return area;
+        }
+        public static void main(String[] args) {
+            Scanner in=new Scanner(System.in);
+            System.out.println("Please input length:");
+            Double a=in.nextDouble();
+            System.out.println("Please input width:");
+            Double b=in.nextDouble();
+            Retangle area=new Retangle(a,b);
+            System.out.println("The lengh is:"+a);
+            System.out.println("The width is:"+b);
+            System.out.println("The area is:"+area.getArea(a,b));
+        }
+    }
+
+
+
---
+++ wj.java
@@ -0,0 +1,33 @@
+package CG;
+import java.util.Scanner;
+public class wj {
+        public static void main(String[] args) {
+            System.out.println("Please input 10 numbers:");
+            int arr[]=new int[10];
+            Scanner in=new Scanner(System.in);
+            for(int i=0;i<arr.length;i++){
+                arr[i] =in.nextInt();
+            }
+            System.out.println("Your numbers are:");
+            for(int i=0;i<arr.length;i++){
+                System.out.print(arr[i]+" ");
+            }
+            System.out.println("\nPlease input m:");
+            int m=in.nextInt();
+            int temp[]=new int[m];
+            int i,j;
+            for(i=arr.length-1,j=m-1;i>arr.length-m||j>=0;i--,j--){//储存在temp
+                temp[j] = arr[i];
+            }
+            for(i=arr.length-1;i>=m;i--){//后移
+                arr[i]=arr[i-m];
+            }
+            for(i=0,j=0;i<m;i++,j++) {//重新赋值
+                arr[i] = temp[j];
+            }
+            System.out.println("The new numbers are:");
+            for(i=0;i<arr.length;i++)
+                System.out.print(arr[i]+" ");
+        }
+}
+
